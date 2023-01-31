0c964fe2df3d5370d3838700c45e461fb0f39609
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-30 23:47:38 -0300

    Método 'createTodo' criado para criar uma tarefa

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
-    public function createTodo() { 
+    public function createTodo(Request $request) { 
+        $array = ['error' => ''];
 
+        // Validando
+        $rules = [
+            'title' => 'required|min:3'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()){
+            $array['error'] = $validator->messages();
+            return $array;
+        }
+
+        $title = $request->input('title');
+
+        // Criando o registro
+        $todo = new Todo();
+        $todo->title = $title;
+        $todo->save();
+
+        return $array;
     }
     
     public function readlAlltodos() { 
