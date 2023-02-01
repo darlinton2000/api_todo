d6969f9efdbd8a7dde13f7d218ee3086c92a856f
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-31 22:01:28 -0300

    Criada a autenticação utilizando Sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -33,6 +35,34 @@ class AuthController extends Controller
         $newUser->password = password_hash($password, PASSWORD_DEFAULT);
         $newUser->token = '';
         $newUser->save();
+
+        return $array;
+    }
+
+    public function login(Request $request){
+        $array = ['error' => ''];
+
+        $creds = $request->only('email', 'password');
+
+        if (Auth::attempt($creds)){
+            $user = User::where('email', $creds['email'])->first();
+
+            $item = time() . rand(0, 99999);
+            $token = $user->createToken($item)->plainTextToken;
+
+            $array['token'] = $token;
+        } else {
+            $array['error'] = 'E-mail e/ou senha incorretos';
+        }
+
+        return $array;
+    }
+
+    public function logout(Request $request){
+        $array = ['error' => ''];
+
+        $user = $request->user();
+        $user->tokens()->delete();
 
         return $array;
     }
--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,17 @@ Route::get('/ping', function(){
     ];
 });
 
-Route::post('/user', [AuthController::class, 'create']);
+Route::get('unauthenticated', function(){
+    return ['error' => 'Usuário não logado!'];
+})->name('login');
 
-Route::post('/todo', [ApiController::class, 'createTodo']);
+Route::post('/user', [AuthController::class, 'create']);
+Route::middleware('auth:sanctum')->get('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->post('/todo', [ApiController::class, 'createTodo']);
 Route::get('/todos', [ApiController::class, 'readlAlltodos']);
 Route::get('/todo/{id}', [ApiController::class, 'readTodo']);
-Route::put('/todo/{id}', [ApiController::class, 'updateTodo']);
-Route::delete('todo/{id}', [ApiController::class, 'deleteTodo']);
+Route::middleware('auth:sanctum')->put('/todo/{id}', [ApiController::class, 'updateTodo']);
+Route::middleware('auth:sanctum')->delete('/todo/{id}', [ApiController::class, 'deleteTodo']);
 
